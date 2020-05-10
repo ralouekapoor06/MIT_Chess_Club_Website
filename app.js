e3629cc786937d534d0400443e3a98788327802c
raloue1 <ralouekapoor1@gmail.com>
2020-05-10 16:57:39 +0530

    Indented the code

--- app.js
+++ app.js
@@ -1,105 +1,63 @@
-const express = require("express"); // express refers to the express framework
+const express = require("express");
 const app = express();
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('51e5a240dc9d4edb930cefab3548dc7c');
-
 console.log(typeof express)
-
-//const bcrypt = require("bcrypt");
-//const saltRounds is 10;
-
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const _ = require("lodash"); // library helps delivering modularity, performance & extras.
-const bodyParser = require("body-parser"); // to get req.body
-
-// configure the port number and tell the server to listen on that port
+const _ = require("lodash");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
-
-app.set("view engine", "ejs"); // set templating engine to ejs
-app.use(bodyParser.urlencoded({extended: true})); // use extended v of body parser
-app.use(express.static("public")); // tell the express app to use the public folder as public
+app.set("view engine", "ejs"); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.use(cookieParser());
 app.use(session({secret: "whydowefallsecretsymbole"}));
 
-// SQL Database connectivity;
 const mysql = require("mysql");
 const db_config = {
-  host: "sql12.freemysqlhosting.net",
-  user:"sql12329649",
-  password: "MYqlqD4qIs",
-  database: "sql12329649"
+  host:"" ,
+  user:"",
+  password:"" ,
+  database: ""
 };
-var db;
-function handleDisconnect() {
-  db = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
 
-  db.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      // console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+var db;
+function handleDisconnect(){
+  db = mysql.createConnection(db_config); 
+  db.connect(function(err) {
+    if(err) { 
+      setTimeout(handleDisconnect, 2000);
     }
     else {
       console.log('MySQL Connected.');
-    }                                 // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
+    }                                 
+  });                                     
   db.on('error', function(err) {
     console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      console.log( err );                                  // server variable configures this)
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                        
+    } else {                                     
+      console.log( err );                                  
     }
   });
 }
-handleDisconnect(); // call the handleDisconnect function once
+handleDisconnect();
 
 
 app.get('/', function(req, res){
     res.render('home');
  });
-
-app.get('/news', function(req, res){
-
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-today = yyyy + '-' + mm + '-' + dd;
-
-var d = new Date();
-d.setDate(d.getDate()-5);
-var dd = String(d.getDate()).padStart(2, '0');
-var mm = String(d.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = d.getFullYear();
-d = yyyy + '-' + mm + '-' + dd;
-var startdate = d
-
-  newsapi.v2.everything({
-    q: 'chess online',
-    from: today,
-    to: startdate,
-    language: 'en',
-    sortBy: 'relevancy',
-    page: 3
-  }).then(response => {
-    console.log(response);
-    response1 = response['articles']
-    //console.log(response1[0]['source'])
-    res.render('news',{data:response1});
-  });
-
-});
  
-app.listen(PORT, function() {
-  console.log(`Server started on port ${PORT}`)
-});
-
-
-
-
 app.get('/store', function(req, res){
   res.render('store');
+});
+
+app.post('/contact',function(req,res){
+  console.log(here);
+  res.render('home');
+})
+
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`)
 });
